d3ad56e8ef545d5cf97714c1e874600bb9416c0a
SergeyStarchenko <pdxserg@gmail.com>
2024-07-23 21:53:19 -0700

    1

--- checkThreeAndTwo.js
+++ checkThreeAndTwo.js
@@ -4,3 +4,23 @@
 //     ["a", "a", "a", "b", "b"] ==> true  // 3x "a" and 2x "b"
 //     ["a", "b", "c", "b", "c"] ==> false // 1x "a", 2x "b" and 2x "c"
 //     ["a", "a", "a", "a", "a"] ==> false // 5x "a"
+
+
+
+function checkThreeAndTwo(array) {
+
+    //your code here
+    let a= array.filter(el => el === "a")
+    let b= array.filter(el => el === "b")
+    let c= array.filter(el => el === "c")
+    return (a.length !==5 && b.length !==1 && c.length !==1
+        &&a.length !==1 && b.length !==1 && c.length !==1
+        &&a.length+b.length+c.length === 5   )
+
+}
+
+console.log(checkThreeAndTwo(["a", "a", "a", "b", "b"]))
+console.log(a.length+b.length+c.length )
+console.log(a.length)
+console.log(b.length)
+console.log(c.length)
