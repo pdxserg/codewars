6595e77e47e828df55d8745a5249720cbc515840
SergeyStarchenko <pdxserg@gmail.com>
2024-08-06 20:19:58 -0700

    1

--- office2.js
+++ office2.js
@@ -23,3 +23,22 @@
 // The Office III - Broken Photocopier
 // The Office IV - Find a Meeting Room
 // The Office V - Find a Chair
+
+function boredom(staff){
+    let s = Object.values(staff)
+    let a =0
+    for(let i = 0; i < s.length; i++){
+        if(s[i]=== "accounts")a =  a+1
+        if(s[i]=== "finance") a = a+2
+        if(s[i]=== "canteen") a = a+10
+        if(s[i]=== "regulation") a = a+3
+        if(s[i]=== "trading"|| s[i]=== "change" ) a = a+6
+        if(s[i]=== "IS") a = a+8
+        if(s[i]=== "retail") a = a+5
+        if(s[i]=== "cleaning") a = a+4
+        if(s[i]=== "pissing about") a = a+ 25
+    }
+    if(a<=80) return 'kill me now'
+    if(a<100) return 'i can handle this'
+    return 'party time!!'
+}
