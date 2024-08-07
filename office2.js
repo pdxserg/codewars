410a115c7595761d0bd00f6dec0cc136922a0fd0
SergeyStarchenko <pdxserg@gmail.com>
2024-08-06 19:32:53 -0700

    1

---
+++ office2.js
@@ -0,0 +1,25 @@
+// the department they work in as values.
+//
+//     Each department has a different boredom assessment score, as follows:
+//
+//     accounts = 1
+// finance = 2
+// canteen = 10
+// regulation = 3
+// trading = 6
+// change = 6
+// IS = 8
+// retail = 5
+// cleaning = 4
+// pissing about = 25
+//
+// Depending on the cumulative score of the team, return the appropriate sentiment:
+//
+//     <=80: 'kill me now'
+// < 100 & > 80: 'i can handle this'
+// 100 or over: 'party time!!'
+//
+// The Office I - Outed
+// The Office III - Broken Photocopier
+// The Office IV - Find a Meeting Room
+// The Office V - Find a Chair
