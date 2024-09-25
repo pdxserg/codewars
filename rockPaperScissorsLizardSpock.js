b286011c6b6c2466be808006562ac528e9520035
SergeyStarchenko <pdxserg@gmail.com>
2024-09-24 23:08:35 -0700

    codeWars

--- rockPaperScissorsLizardSpock.js
+++ rockPaperScissorsLizardSpock.js
@@ -17,17 +17,3 @@
 // Values will be given as one of "rock", "paper", "scissors", "lizard", "spock".
 
 
-function rpsls(pl1, pl2) {
-    if (pl1 === pl2) return 'Draw!'
-    let rules = {
-        'paper': ['rock', 'spock'],
-        'rock': ['lizard', 'scissors'],
-        'lizard': ['spock', 'paper'],
-        'spock': ['scissors', 'rock'],
-        'scissors': ['lizard', 'paper']
-    }
-    if (rules[pl1].includes(pl2))
-        return 'Player 1 Won!'
-    else
-        return 'Player 2 Won!'
-}
