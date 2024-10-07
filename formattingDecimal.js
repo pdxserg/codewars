0e29914978d879278c695c6e1eb493b2cb16205c
SergeyStarchenko <pdxserg@gmail.com>
2024-10-06 20:49:29 -0700

    codeWars

--- formattingDecimal.js
+++ formattingDecimal.js
@@ -13,3 +13,13 @@ function twoDecimalPlaces(n) {
     // Your code here
     return Math.round(n*100)/100
 }
+
+const Test = require('@codewars/test-compat');
+
+describe("twoDecimalPlaces", function() {
+    it("works for some examples", function() {
+        Test.assertEquals(twoDecimalPlaces(4.659725356), 4.66, "didn't work for 4.659725356");
+        Test.assertEquals(twoDecimalPlaces(173735326.3783732637948948), 173735326.38, "didn't work for 173735326.3783732637948948");
+        Test.assertEquals(twoDecimalPlaces(4.653725356), 4.65, "didn't work for 4.653725356");
+    });
+});
